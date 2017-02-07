37b5c5490fc02a46aee183f357f2a9373c536eb5
Keisuke Kobayashi <kkobayashi@coubic.com>
2017-02-08 01:25:30 +0900

    Fix lint

--- sample/src/main/java/com/kobakei/ratethisapp/sample/MainActivity.java
+++ sample/src/main/java/com/kobakei/ratethisapp/sample/MainActivity.java
@@ -15,15 +15,13 @@
  */
 package com.kobakei.ratethisapp.sample;
 
-import com.kobakei.ratethisapp.RateThisApp;
-
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.kobakei.ratethisapp.RateThisApp;
 
 /**
  * Sample application of RateThisApp
@@ -89,13 +87,6 @@ public class MainActivity extends AppCompatActivity {
                 RateThisApp.stopRateDialog(MainActivity.this);
             }
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
     }
 
     @Override
